e0acdcc5a7223730ef8f84e90ed662492f586ce8
pieter <pumpkinpieter@gmail.com>
2022-05-19 12:11:19 -0700

    Fixing some issues with arf scalar convergence folder.

--- arf_fiber/scalar_modes/graph.py
+++ arf_fiber/scalar_modes/graph.py
@@ -14,74 +14,39 @@ SMALL_SIZE = 14
 MEDIUM_SIZE = 18
 BIGGER_SIZE = 22
 
-plt.rc('font', size=MEDIUM_SIZE)          # controls default text sizes
+plt.rc('font', size=MEDIUM_SIZE)         # controls default text sizes
 plt.rc('axes', titlesize=BIGGER_SIZE)    # fontsize of the axes title
 plt.rc('axes', labelsize=MEDIUM_SIZE)    # fontsize of the x and y labels
 plt.rc('xtick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
 plt.rc('ytick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
-plt.rc('legend', fontsize=MEDIUM_SIZE)    # legend fontsize
+plt.rc('legend', fontsize=MEDIUM_SIZE)   # legend fontsize
 plt.rc('figure', titlesize=BIGGER_SIZE)  # fontsize of the figure title
 
 
-path = os.path.relpath(
-    os.path.expanduser('~/local/convergence/\
-arf_fiber/vector_modes/outputs'))
-
-betas = np.load(path + '/betas.npy').imag
-dofs = np.load(path + '/dofs.npy')
-
-# Filter out bad values
-
-B = np.where(betas != 0, betas, 1e99)
-BB = np.min(B, axis=2)
-BBB = np.where(BB != 1e99, BB, 0)
-
-CL = 20 * BBB / np.log(10)
+main = os.path.expanduser('~/local/convergence/arf_fiber/scalar_modes/outputs')
+path = os.path.relpath(main)
 
 plt.figure(figsize=(18, 16))
 
-for i in range(0, CL.shape[0]-1):
-    plt.plot(np.log(dofs[i, :]), CL[i, :],
-             'o-', label='ref='+str(i),
+for r in range(1):
+    betas = np.load(path + '/ref'+str(r)+'all_betas.npy')
+    dofs = np.load(path + '/ref'+str(r)+'all_dofs.npy')
+
+    # Filter out bad values
+
+    B = np.where(betas != 0, betas, 1e99)
+    BB = np.min(B, axis=1)
+
+    CL = 20 * BB / np.log(10)
+    plt.plot(dofs, CL, 'o-', label='ref='+str(r),
              linewidth=2.5, markersize=8)
-
-betas = np.load(path + '/betas2.npy').imag
-dofs = np.load(path + '/dofs2.npy')
-
-# Filter out bad values
-
-B = np.where(betas != 0, betas, 1e99)
-BB = np.min(B, axis=2)
-BBB = np.where(BB != 1e99, BB, 0)
-
-CL = 20 * BBB / np.log(10)
-
-
-for i in range(0, CL.shape[0]):
-    plt.plot(np.log(dofs[i, :]), CL[i, :],
-             'o-', label='ref='+str(2),
-             linewidth=2.5, markersize=8)
-
-betas = np.load(path + '/betas3.npy').imag
-dofs = np.load(path + '/dofs3.npy')
-
-# Filter out bad values
-
-B = np.where(betas != 0, betas, 1e99)
-BB = np.min(B, axis=2)
-BBB = np.where(BB != 1e99, BB, 0)
-
-CL = 20 * BBB / np.log(10)
-
-
-for i in range(0, CL.shape[0]):
-    plt.plot(np.log(dofs[i, :]), CL[i, :],
-             'o-',
-             linewidth=2.5, markersize=8)
-
 
 plt.legend()
 
-plt.xlabel('log of ndofs')
-plt.ylabel('log of error')
+plt.xlabel('ndofs')
+plt.ylabel('CL')
+plt.title('Arf Poletti Scalar convergence.')
+plt.yscale('log')
+plt.xscale('log')
 plt.grid()
+plt.show()
