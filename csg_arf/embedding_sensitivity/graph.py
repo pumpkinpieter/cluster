3331b2d5ccd2db4ebf066280928d455a68432c2f
pieter <pumpkinpieter@gmail.com>
2022-05-27 10:39:33 -0700

    Graph for embedding.

--- csg_arf/embedding_sensitivity/graph.py
+++ csg_arf/embedding_sensitivity/graph.py
@@ -12,43 +12,41 @@ import matplotlib.pyplot as plt
 
 plt.close()
 
-SMALL_SIZE = 14
-MEDIUM_SIZE = 18
-BIGGER_SIZE = 22
+SMALL_SIZE = 20
+MEDIUM_SIZE = 20
+BIGGER_SIZE = 30
 
-plt.rc('font', size=MEDIUM_SIZE)          # controls default text sizes
+plt.rc('font', size=MEDIUM_SIZE)         # controls default text sizes
 plt.rc('axes', titlesize=BIGGER_SIZE)    # fontsize of the axes title
 plt.rc('axes', labelsize=MEDIUM_SIZE)    # fontsize of the x and y labels
 plt.rc('xtick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
 plt.rc('ytick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
-plt.rc('legend', fontsize=MEDIUM_SIZE)    # legend fontsize
+plt.rc('legend', fontsize=MEDIUM_SIZE)   # legend fontsize
 plt.rc('figure', titlesize=BIGGER_SIZE)  # fontsize of the figure title
 
+path = os.path.relpath(os.path.expanduser('~/local/convergence/csg_arf/\
+embedding_sensitivity/outputs'))
 
-path = os.path.relpath(
-    os.path.expanduser('~/local/convergence/\
-arf_fiber/embedding_sensitivity/outputs'))
-
-plt.figure(figsize=(18, 16))
+plt.figure(figsize=(22, 16))
 
 raw = np.load(path + '/all_e.npy').imag
-es = np.linspace(0.001, 1, 40)
+es = np.linspace(0.002, .9999, 80)
 
 base = np.zeros_like(es)
 
-for j in range(len(es)-1):
+for j in range(len(es)):
     b = raw[j, :]
-    c = np.where((b != 0) * (np.abs(b) < 1) * (b > 0), 1, 0)
+    c = np.where((b != 0) * (np.abs(b) < 10) * (b > 0), 1, 0)
     base[j] = np.mean(b, where=list(c))
 
 CL = 20 * base / np.log(10)
 
-plt.plot(1-es[1:], CL[1:], 'o-', linewidth=2.5, markersize=8)
+plt.plot(1-es, CL, 'o-', linewidth=2.5, markersize=8)
 
-# plt.legend()
+plt.title("Embedding Sensitivity Vector Method.\n")
 
-plt.xlabel('log of ndofs')
-plt.ylabel('log of error')
+plt.xlabel("\nFraction of Capillary Tube Embedded")
+plt.ylabel("CL")
 plt.yscale('log')
 # plt.xscale('log')
 plt.grid()
