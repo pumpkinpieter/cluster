067a52bf725361e8fb6565835871d4714d35d2bc
pieter <pumpkinpieter@gmail.com>
2022-05-29 00:16:59 -0700

    minor

--- csg_arf/embedding_sensitivity/graph.py
+++ csg_arf/embedding_sensitivity/graph.py
@@ -12,13 +12,22 @@ import matplotlib.pyplot as plt
 
 plt.close()
 
-plt.rc('axes', titlesize=30)    # fontsize of the axes title
-plt.rc('figure', titlesize=30)  # fontsize of the figure title
+SMALL_SIZE = 18
+MEDIUM_SIZE = 25
+BIGGER_SIZE = 40
+
+plt.rc('font', size=MEDIUM_SIZE)         # controls default text sizes
+plt.rc('axes', titlesize=BIGGER_SIZE)    # fontsize of the axes title
+plt.rc('axes', labelsize=MEDIUM_SIZE)    # fontsize of the x and y labels
+plt.rc('xtick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
+plt.rc('ytick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
+plt.rc('legend', fontsize=MEDIUM_SIZE)   # legend fontsize
+plt.rc('figure', titlesize=BIGGER_SIZE)  # fontsize of the figure title
 
 path = os.path.relpath(os.path.expanduser('~/local/convergence/csg_arf/\
 embedding_sensitivity/outputs'))
 
-plt.figure(figsize=(22, 16))
+plt.figure(figsize=(30, 16))
 
 raw = np.load(path + '/all_e.npy').imag
 es = np.linspace(0.002, .9999, 240)
@@ -27,7 +36,7 @@ base = np.zeros_like(es)
 
 for j in range(len(es)):
     b = raw[j, :]
-    c = np.where((b != 0) * (np.abs(b) < 20) * (b > 0), 1, 0)
+    c = np.where((b != 0) * (np.abs(b) < 2) * (b > 0), 1, 0)
     base[j] = np.mean(b, where=list(c))
 
 CL = 20 * base / np.log(10)
@@ -35,10 +44,10 @@ CL = 20 * base / np.log(10)
 plt.plot(1-es, CL, 'o-', linewidth=2.5, markersize=8)
 
 plt.title("Embedding Sensitivity Vector Method.\n")
+plt.xticks(np.linspace(0, 1, 21))
 
 plt.xlabel("\nFraction of Capillary Tube Embedded")
 plt.ylabel("CL")
 plt.yscale('log')
-# plt.xscale('log')
 plt.grid()
 plt.show()
