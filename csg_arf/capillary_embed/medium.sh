4a14632204d0d10430d9f2c5dee3684b7bbb3f27
Pieter Vandenberge <piet2@login1.cluster>
2022-05-28 19:05:03 -0700

    Modified slurm script to give individual log and error files.

--- csg_arf/capillary_embed/medium.sh
+++ csg_arf/capillary_embed/medium.sh
@@ -1,14 +1,11 @@
 #!/usr/bin/bash
 #SBATCH --job-name arfemb 
-#SBATCH -N 60
-#SBATCH -n 60
+#SBATCH -N 1
+#SBATCH -n 1
 #SBATCH --tasks-per-node 1
 #SBATCH --cpus-per-task 20
 #SBATCH --partition medium
 #SBATCH --mem 0
-#SBATCH --mail-user piet2@pdx.edu
-#SBATCH --output=logs/e%a.log
-#SBATCH --error=errors/e%a.err
 
 # Load needed modules.
 module load ngsolve/serial
@@ -18,8 +15,10 @@ module load intel
 # Run the code.
 echo "Starting convergence study: "
 date
-for i in {0..239}
+for i in {10..10}
     do
-        srun --exclusive --nodes 1 --ntasks 1 python3 emb_modes.py 1 8 ${i} &
+        srun --exclusive --nodes 1 --ntasks 1 \
+            --output="logs/e_${i}.out" --error="errors/e_${i}.err" \
+            python3 emb_modes.py 1 8 ${i} 0 1 
 done
 wait
