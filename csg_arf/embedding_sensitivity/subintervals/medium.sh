52d01a705e7726d37205163a7b64270b59bda6a1
Pieter Vandenberge <piet2@login1.cluster>
2022-05-29 11:34:52 -0700

    Restored graph (git is dumb) and driver script.

--- csg_arf/embedding_sensitivity/subintervals/medium.sh
+++ csg_arf/embedding_sensitivity/subintervals/medium.sh
@@ -1,13 +1,9 @@
 #!/usr/bin/bash
 #SBATCH --job-name arfemb 
-#SBATCH -N 80
-#SBATCH -n 80
 #SBATCH --tasks-per-node 1
 #SBATCH --cpus-per-task 20
 #SBATCH --partition medium
 #SBATCH --mem 0
-#SBATCH --output=logs/log.out
-#SBATCH --error=errors/log.out
 
 # Load needed modules.
 module load ngsolve/serial
@@ -26,11 +22,16 @@ N=1
 l=0
 h=1
 
+#SBATCH -N $N
+#SBATCH -n $N
+#SBATCH --output=inverval_${L}_${R}/logs/log.out
+#SBATCH --error=inverval_${L}_${R}/errors/log.out
+
 for i in {$L..$R}
     do
         srun --exclusive --nodes 1 --ntasks 1 \
-            --output="logs/e_${i}.out" \
-            --error="errors/e_${i}.err" \
+            --output="inverval_${L}_${R}/logs/e_${i}.out" \
+            --error="inverval_${L}_${R}/errors/e_${i}.err" \
             python3 sub_modes.py $ref $p ${i} $L $R $N $l $h &
 done
 wait
