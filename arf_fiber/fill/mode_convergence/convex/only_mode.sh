eb7ab7deb423a098eeeab97ffc0aeca34badba83
pieter <pumpkinpieter@gmail.com>
2022-07-11 19:22:57 -0700

    Setting up fill study further: will do initial inquiry into flat, convex and concave fills.  For the flat one, I'll do 3 depths

---
+++ arf_fiber/fill/mode_convergence/convex/only_mode.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/bash
+#SBATCH --job-name getmode 
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH --tasks-per-node 1
+#SBATCH --cpus-per-task 20
+#SBATCH --partition medium 
+#SBATCH --mem 0
+#SBATCH --mail-user piet2@pdx.edu
+#SBATCH --output=logs/getmode.log
+#SBATCH --error=errors/getmode.err
+
+# Load needed modules.
+module load ngsolve/serial
+module load gcc-9.2.0
+module load intel
+
+# Run the code.
+echo "Starting convergence study: "
+srun --exclusive --nodes 1 --ntasks 1 python3 get_mode.py 0 14 
