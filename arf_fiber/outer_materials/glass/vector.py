0c38df2df4444b26d84299e17cd6099f081523ff
pieter <pumpkinpieter@gmail.com>
2022-07-27 16:12:46 -0700

    Setting up drivers for outer materials studies: adding correct search centers, fixing maxh in polymer to account for ripples there.

--- arf_fiber/outer_materials/glass/vector.py
+++ arf_fiber/outer_materials/glass/vector.py
@@ -40,27 +40,19 @@ outer_materials = [
     #  'T': T_hard_polymer,
     #  'maxh': 2},
 
-    {'material': 'buffer',
-     'n': n0,
-     'T': T_buffer,
-     'maxh': 2},
+    # {'material': 'buffer',
+    #  'n': n0,
+    #  'T': T_buffer,
+    #  'maxh': .05},
 
     {'material': 'Outer',
      'n': n0,
      'T': T_outer,
-     'maxh': 4}
+     'maxh': .06}
 ]
 
-# Center, radius and span
-center = 5.066
-radius = .1
-nspan = 4
-npts = 4
-
-# PML strength
-alpha = 5
-
 # Set result arrays
+nspan = 4
 betas = np.zeros(nspan, dtype=complex)
 dofs = np.zeros(1, dtype=float)
 
@@ -68,8 +60,20 @@ if __name__ == '__main__':
 
     ref, p = int(sys.argv[1]), int(sys.argv[2])
 
-    a = ARF2(refine=ref, curve=max(p+1, 3), poly_core=True,
+    a = ARF2(refine=ref, curve=max(p+1, 3),
+             name='fine_cladding',
+             poly_core=True,
              outer_materials=outer_materials)
+
+    center = a.L**2 * a.k**2 * (n0**2 - n_air**2) + 5.066
+
+    # Center, radius and span
+    center = center
+    radius = .1
+    npts = 4
+
+    # PML strength
+    alpha = 5
 
     print('\n' + '#'*8 + ' refinement: ' + str(ref) +
           ', degree: ' + str(p) + '  ' + '#'*8 + '\n', flush=True)
