e0401a69a392afe1c47236f253b72defe8d74d15
pieter <pumpkinpieter@gmail.com>
2022-07-28 18:33:00 -0700

    Added polynomial degree markers to convergence plot for fine cladding case.

--- arf_fiber/poletti/vector_modes/fine_cladding/graph.py
+++ arf_fiber/poletti/vector_modes/fine_cladding/graph.py
@@ -31,6 +31,9 @@ path = os.path.relpath(main)
 
 plt.figure(figsize=(18, 16))
 
+fig = plt.gcf()
+ax = plt.gca()
+
 for r in range(2):
     betas = np.load(path + '/ref'+str(r)+'all_betas.npy').imag
     dofs = np.load(path + '/ref'+str(r)+'all_dofs.npy')
@@ -41,17 +44,44 @@ for r in range(2):
     BB = np.min(B, axis=1)
 
     CL = 20 * BB / np.log(10)
-    plt.plot(dofs[1:], CL[1:], 'o-', label='ref='+str(r),
-             linewidth=2.5, markersize=8)
+    ax.plot(dofs, CL, 'o-', label='refinements: '+str(r),
+            linewidth=2.5, markersize=9,  markerfacecolor='white')
+
+    for i, dc in enumerate(zip(dofs, CL)):
+        if r == 0:
+            ax.annotate('p='+str(i), xy=dc, xytext=(-40, -50),
+                        textcoords='offset points',
+                        arrowprops=dict(arrowstyle="-",
+                        connectionstyle="arc3")
+                        )
+        elif r == 1:
+            ax.annotate('p=' + str(i), xy=dc, xytext=(0, 40),
+                        textcoords='offset points',
+                        arrowprops=dict(arrowstyle="-",
+                        connectionstyle="arc3")
+                        )
+
+xmin, xmax = ax.get_xlim()
+
+ax.plot([xmin, xmax], [.153, .153], linestyle='dashdot', color='gray')
+
 
 plt.legend()
 
 plt.xlabel('ndofs')
 plt.ylabel('CL')
-plt.title('CSG Arf Poletti Vector Convergence, HE11 mode.\n Glass cladding, \
-air in outer region, no polymer.  \n limit CL =~ .153')
+
+plt.title('Arf Fundamental Mode Convergence\n No polymer, \
+air in outer region.\n')
+
 plt.yscale('log')
 plt.xscale('log')
+
+plt.yticks([.1, .153, 1, 10], labels=['.1', 'lim CL =.153', '1', '10'])
 plt.xticks([10**5, 10**6])
-plt.grid()
+
+plt.grid(which='major', axis='y')
+plt.grid(which='major', axis='x')
+plt.grid(which='minor', axis='x', linestyle='--', linewidth=.5)
+
 plt.show()
