9c4cf96ab8a559806e51c0f7840355fcf98e0472
pieter <pumpkinpieter@gmail.com>
2022-08-04 15:57:47 -0700

    Finished shifting tubes extra glass study setup.:

--- arf_fiber/embedding/extra_glass/twenty/shifting_tubes/emb_modes.py
+++ arf_fiber/embedding/extra_glass/twenty/shifting_tubes/emb_modes.py
@@ -14,27 +14,42 @@ if not os.path.isdir('modes'):
     print('Making directory: modes')
     os.makedirs('modes')
 
-# Center, radius and span
-center0 = 5.066
-radius0 = .1
+# Set outer materials
+scaling = 15
+n_glass = 1.4388164768221814
+n_air = 1.00027717
+
+T_extra = 20 / scaling
+T_buffer = 10 / scaling
+T_outer = 10 / scaling
+n0 = n_air  # Sets buffer and outer region refractive index.
+
+outer_materials = [
+    {'material': 'extra_glass',
+     'n': n_glass,
+     'T': T_extra,
+     'maxh': .05},
+
+    {'material': 'buffer',
+     'n': n0,
+     'T': T_buffer,
+     'maxh': .1},
+
+    {'material': 'Outer',
+     'n': n0,
+     'T': T_outer,
+     'maxh': 1}
+]
+
+# Set result arrays
 nspan = 4
-npts = 4
+betas = np.zeros(nspan, dtype=complex)
 
 # Embedding parameter array
-
 E = np.linspace(0.002, .9999, 240)
-
-# Linear fit for finding search centers
-m, b = -0.2758566730320395, 5.07341374920433
-
-# Seach centers
-centers = b + m * E
 
 # PML strength
 alpha = 5
-
-# Set result arrays
-betas = np.zeros(nspan, dtype=complex)
 
 if __name__ == '__main__':
 
@@ -45,17 +60,18 @@ if __name__ == '__main__':
         save_index = np.where((L < E) * (E < R))[0]
 
     a = ARF2(name='fine_cladding', poly_core=True, refine=ref,
-             curve=max(p+1, 8), shift_capillaries=True, e=E[i])
+             curve=max(p+1, 8), shift_capillaries=True, e=E[i],
+             outer_materials=outer_materials)
 
     print('\n' + '#'*8 + ' refinement: ' + str(ref) +
           ', degree: ' + str(p) + ', e: ' + str(E[i]) + '#'*8 + '\n',
           flush=True)
-    if E[i] < .4:
-        center = center0
-        radius = radius0
-    else:
-        center = centers[i]
-        radius = .007
+
+    center = a.L**2 * a.k**2 * (n0**2 - n_air**2) + 5.066
+    radius = .007
+    npts = 4
+    alpha = 5
+
     beta, _, Es, _, _ = a.leakyvecmodes(ctr=center,
                                         rad=radius,
                                         alpha=alpha,
