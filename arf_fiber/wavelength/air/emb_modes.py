9da14328d7212eb4c1e691ec2a47217c55417201
pieter <pumpkinpieter@gmail.com>
2022-10-05 10:22:51 -0700

    Working on driver file for wavelength embedding still. Might be done with this commit.

--- arf_fiber/wavelength/air/emb_modes.py
+++ arf_fiber/wavelength/air/emb_modes.py
@@ -18,15 +18,21 @@ if not os.path.isdir('modes'):
 nspan = 4
 betas = np.zeros(nspan, dtype=complex)
 
+# Center interpolation
+wls = np.linspace(.5, 2, 200) * 1e-6
+xs = np.array([wls[65], wls[75], wls[100], wls[125],
+              wls[150], wls[175], wls[199]])
+ys = np.array([4.7, 4.786, 4.9, 4.967, 5.018, 5.071, 5.12])
+a, b, c, d = np.polyfit(xs, ys, 3)
+
 # Embedding parameter array
-wls = np.linspace(0.5, 2, 200) * 1e-6
+wls = np.linspace(1, 2, 200) * 1e-6
+
+# Search centers
+centers = a * wls**3 + b * wls**2 + c * wls + d
 
 # PML strength
 alpha = 5
-
-center = 4.7
-radius = .1
-npts = 4
 
 if __name__ == '__main__':
 
@@ -42,6 +48,10 @@ if __name__ == '__main__':
     print('\n' + '#'*8 + ' refinement: ' + str(ref) +
           ', degree: ' + str(p) + ', wavelength: ' + str(wls[i]) +
           '#'*8 + '\n', flush=True)
+
+    center = centers[i]
+    radius = .05
+    npts = 4
 
     beta, _, Es, _, _ = a.leakyvecmodes(ctr=center,
                                         rad=radius,
