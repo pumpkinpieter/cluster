b06a4153dbabeff62d5ea1236570925cef7067c2
Pieter Vandenberge <pumpkinpieter@gmail.com>
2022-10-04 21:43:20 -0700

    Graph and mesh viewer changes in outer material embedding studies.

--- arf_fiber/embedding/soft_hard_air/fixed_tubes/graph.py
+++ arf_fiber/embedding/soft_hard_air/fixed_tubes/graph.py
@@ -14,7 +14,7 @@ from matplotlib.ticker import (AutoMinorLocator, MultipleLocator)
 plt.close('all')
 
 main = os.path.expanduser('~/local/convergence/arf_fiber/embedding/')
-path = os.path.relpath(main + 'hard_hard/fixed_tubes/outputs')
+path = os.path.relpath(main + 'soft_hard_air/fixed_tubes/outputs')
 
 raw = np.load(path + '/all_e.npy').imag
 es = np.linspace(0.002, .9999, 240)
@@ -31,26 +31,26 @@ for j in range(len(es)):
 CL = 20 * base / np.log(10)
 
 # Set up the figure and subplots
-fig, (ax1) = plt.subplots(1, 1, sharex=False, figsize=(30, 15))
+fig, (ax1) = plt.subplots(1, 1, sharex=False, figsize=(16, 8))
 
 # Plot the data
 ax1.plot(es, CL, '^-', color='blue',
          label='shifting_capillaries',
-         linewidth=2.5, markersize=3.4)
+         linewidth=1.5, markersize=2.4)
 # Set Figure and Axes parameters ################################
 
 # Set titles
 fig.suptitle("Embedding Sensitivity: Fixed Capillaries, \
- \n Hard Polymer Coating, PML in polymer", fontsize=30)
+ \n Soft and Hard Polymer, PML in air", fontsize=20)
 
 # Set axis labels
-ax1.set_xlabel("\nFraction of Capillary Tube Embedded", fontsize=20)
-ax1.set_ylabel("CL", fontsize=25)
+ax1.set_xlabel("\nFraction of Capillary Tube Embedded", fontsize=16)
+ax1.set_ylabel("CL", fontsize=16)
 
 # Set up ticks and grids
 
-plt.rc('xtick', labelsize=16)
-plt.rc('ytick', labelsize=16)
+plt.rc('xtick', labelsize=10)
+plt.rc('ytick', labelsize=10)
 
 ax1.xaxis.set_major_locator(MultipleLocator(.05))
 ax1.xaxis.set_minor_locator(AutoMinorLocator(5))
@@ -61,17 +61,6 @@ ax1.grid(which='minor', color='#CCCCCC', linestyle=':')
 
 # # Set log scale on y axes
 ax1.set_yscale('log')
-
-# Turn on subplot tool when graphing to allow finer control of spacing
-# plt.subplot_tool(fig)
-
-# After fine tuning, these are the values we want (use export from tool)
-plt.subplots_adjust(top=0.905,
-                    bottom=0.11,
-                    left=0.065,
-                    right=0.95,
-                    hspace=0.2,
-                    wspace=0.2)
 
 # Show figure (needed for running from command line)
 plt.show()
