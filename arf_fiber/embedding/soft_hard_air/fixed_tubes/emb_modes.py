07937701344d137701092bade8eac2b4b8e555f6
Pieter Vandenberge <piet2@login1.cluster>
2022-10-04 19:45:55 -0700

    Cluster changes.

--- arf_fiber/embedding/soft_hard_air/fixed_tubes/emb_modes.py
+++ arf_fiber/embedding/soft_hard_air/fixed_tubes/emb_modes.py
@@ -79,7 +79,7 @@ if __name__ == '__main__':
           flush=True)
 
     center = a.L**2 * a.k**2 * (n0**2 - n_air**2) + 5.066
-    radius = .1
+    radius = .01
     npts = 4
     alpha = 5
 
--- arf_fiber/embedding/soft_hard_air/fixed_tubes/medium.sh
+++ arf_fiber/embedding/soft_hard_air/fixed_tubes/medium.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/bash
 #SBATCH --job-name arfemb 
-#SBATCH -N 60
-#SBATCH -n 60
+#SBATCH -N 6
+#SBATCH -n 6
 #SBATCH --tasks-per-node 1
 #SBATCH --cpus-per-task 20
 #SBATCH --partition medium
 #SBATCH --mem 0
 #SBATCH --output=logs/log.out
 #SBATCH --error=errors/log.out
+#SBATCH --mail-user=piet2@pdx.edu
+#SBATCH --mail-type=ALL
 
 # Load needed modules.
 module load ngsolve/serial
@@ -20,8 +22,8 @@ date
 for i in {0..239}
     do
         srun --exclusive --nodes 1 --ntasks 1 \
-            --output="logs/e_${i}.out" \
-            --error="errors/e_${i}.err" \
-            python3 emb_modes.py 0 5 ${i} &
+            --output="logs/e_${i}_task%s.out" \
+            --error="errors/e_${i}_task%s.err" \
+            python3 emb_modes.py 0 3 ${i} &
 done
 wait
