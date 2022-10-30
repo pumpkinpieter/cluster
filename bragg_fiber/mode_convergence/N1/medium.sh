632faab23aab34a041c7b90a5392dc7f4ffbfaf1
pieter <pumpkinpieter@gmail.com>
2022-10-30 00:00:15 -0700

    Setting up mode convergence studies.

---
+++ bragg_fiber/mode_convergence/N1/medium.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/bash
+#SBATCH --job-name funarf 
+#SBATCH -N 18
+#SBATCH -n 18
+#SBATCH --tasks-per-node 1
+#SBATCH --cpus-per-task 20
+#SBATCH --partition medium
+#SBATCH --mem 0
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user piet2@pdx.edu
+#SBATCH --output=logs/log.out
+#SBATCH --error=errors/error.err
+
+# Load needed modules.
+module load ngsolve/serial
+module load gcc-9.2.0
+module load intel
+
+indx=1
+# Run the code.
+echo "Starting convergence study: "
+for i in {0..7}
+    do
+        srun --unbuffered --nodes 1 --ntasks 1 \
+           --output="logs/ref0_p${i}_task%s.out" \
+           --error="errors/ref0_p${i}_task%s.err" \
+           python3 driver.py 0 ${i} $indx &
+done
+
+for j in {0..5}
+    do
+        srun --unbuffered --nodes 1 --ntasks 1 \
+           --output="logs/ref1_p${j}_task%s.out"\
+           --error="errors/ref1_p${j}_task%s.err"\
+           python3 driver.py 1 ${j} $indx &
+done
+
+for k in {0..3}
+    do
+        srun --unbuffered --nodes 1 --ntasks 1 \
+           --output="logs/ref2_p${k}_task%s.out"\
+           --error="errors/re2_p${k}_task%s.err"\
+           python3 driver.py 2 ${k} $indx &
+done
+wait
