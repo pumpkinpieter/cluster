0a9384c80fd896b1ca348acff00823e211e03c15
pieter <pumpkinpieter@gmail.com>
2022-11-28 21:31:41 -0800

    driver changes.

--- arf_fiber/kolyadin/modes/polymer/get_mode.py
+++ arf_fiber/kolyadin/modes/polymer/get_mode.py
@@ -20,6 +20,17 @@ radius = .05      # search radius
 nspan = 4
 npts = 4
 
+n_poly = 1.5 - .1j
+n_buffer = 1.00027717
+n0 = n_buffer
+
+scaling = 59.5
+T_cladding = 1.2 * 25.5 / scaling
+
+T_poly = .5 * T_cladding
+T_buffer = .5 * T_cladding
+T_outer = .75 * T_cladding
+
 # PML strength
 alpha = 5
 
@@ -31,8 +42,26 @@ if __name__ == '__main__':
 
     ref, p = int(sys.argv[1]), int(sys.argv[2])
 
+    outer_materials = [
+
+        {'material': 'poly',
+         'n': n_poly,
+         'T': T_poly,
+         'maxh': .03},
+
+        {'material': 'buffer',
+         'n': n_buffer,
+         'T': T_buffer,
+         'maxh': .2},
+
+        {'material': 'Outer',
+         'n': n0,
+         'T': T_outer,
+         'maxh': .2}
+    ]
+
     a = ARF2(name='kolyadin', refine=ref, curve=max(p+1, 8), poly_core=True,
-             wl=3.25e-6)
+             wl=3.25e-6, outer_materials=outer_materials)
 
     print('\n' + '#'*8 + ' refinement: ' + str(ref) +
           ', degree: ' + str(p) + '  ' + '#'*8 + '\n', flush=True)
