f65666e36e9f56daad2909eef8a0b634b9f0e2cb
Pieter Vandenberge <piet2@login1.cluster>
2022-12-26 22:08:53 -0800

    Remote changes.

--- arf_fiber/embedding/polymer/long.sh
+++ arf_fiber/embedding/polymer/long.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/bash
 #SBATCH --job-name arfemb 
-#SBATCH -N 37
-#SBATCH -n 37
+#SBATCH -N 25
+#SBATCH -n 25
 #SBATCH --tasks-per-node 1
 #SBATCH --cpus-per-task 20
-#SBATCH --partition medium
+#SBATCH --partition long
 #SBATCH --mem 0
-#SBATCH --output=logs/log.out
-#SBATCH --error=errors/log.out
+#SBATCH --output=logs/log2.out
+#SBATCH --error=errors/log2.out
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user piet2@pdx.edu
 
@@ -16,18 +16,17 @@ module load ngsolve/serial
 module load gcc-9.2.0
 module load intel
 
-rm logs/*task*
-rm errors/*task*
+#rm logs/*task*
+#rm errors/*task*
 
 # Run the code.
 echo "Starting convergence study: "
 date
-for i in {0..599}
+for i in {0..239}
     do
         srun --unbuffered --nodes 1 --ntasks 1 \
             --output="logs/2e_${i}_task%s.out" \
             --error="errors/2e_${i}_task%s.err" \
-            python3 driver600.py 0 4 ${i} 0.0005 &
-
+            python3 driver240.py 0 4 ${i} 0.005 &
 done
 wait
