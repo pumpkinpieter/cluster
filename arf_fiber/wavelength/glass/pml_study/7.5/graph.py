1fb8195ab38c7853d36db9654e1ea121638d04c5
pieter <pumpkinpieter@gmail.com>
2023-02-23 16:07:07 -0800

    Cleaning up flow of pml stability studies.

---
+++ arf_fiber/wavelength/glass/pml_study/7.5/graph.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 19 20:33:33 2022
+
+@author: pv
+"""
+
+import numpy as np
+from os.path import relpath, expanduser
+import matplotlib.pyplot as plt
+from matplotlib.ticker import (AutoMinorLocator, MultipleLocator)
+
+plt.close('all')
+
+main = expanduser('~/local/convergence/arf_fiber/wavelength/glass/pml_study')
+path = relpath(main + '/7.5/outputs')
+
+raw = np.load(path + '/p4_alpha7.50_all_wl.npy').imag
+
+wls = np.linspace(1, 2, 200) * 1e-6
+base = np.zeros_like(wls)
+
+for j in range(len(wls)):
+
+    b = raw[j, :]
+
+    c = np.where((b != 0), 1, 0)
+    base[j] = np.mean(b, where=list(c))
+
+
+CL = 20 * base / np.log(10)
+
+# Set up the figure and subplots
+fig, (ax1) = plt.subplots(1, 1, sharex=False, figsize=(15, 7))
+
+# Plot the data
+ax1.plot(wls, CL, '^-', color='blue',
+         label='shifting_capillaries',
+         linewidth=1.5, markersize=1.4)
+# Set Figure and Axes parameters ################################
+
+# Set titles
+fig.suptitle("Wavelength Study: glass cladding to infinity",  fontsize=16)
+
+# Set axis labels
+ax1.set_xlabel("\nWavelength", fontsize=11)
+ax1.set_ylabel("CL", fontsize=11)
+
+# Set up ticks and grids
+
+plt.rc('xtick', labelsize=8)
+plt.rc('ytick', labelsize=8)
+
+ax1.xaxis.set_major_locator(MultipleLocator(1e-7))
+ax1.xaxis.set_minor_locator(AutoMinorLocator(5))
+ax1.yaxis.set_major_locator(MultipleLocator(1))
+ax1.yaxis.set_minor_locator(AutoMinorLocator(1))
+ax1.grid(which='major', color='#CCCCCC', linewidth=1.2, linestyle='--')
+ax1.grid(which='minor', color='#CCCCCC', linestyle=':')
+
+# # Set log scale on y axes
+ax1.set_yscale('log')
+
+# Turn on subplot tool when graphing to allow finer control of spacing
+# plt.subplot_tool(fig)
+
+# After fine tuning, these are the values we want (use export from tool)
+plt.subplots_adjust(top=0.905,
+                    bottom=0.11,
+                    left=0.065,
+                    right=0.95,
+                    hspace=0.2,
+                    wspace=0.2)
+
+# Show figure (needed for running from command line)
+plt.show()
+
+# %%
+
+# Save cleaned data to numpy arrays for comparison plot
+
+np.save(relpath(main + 'data/poletti_N0_CLs'), CL)
+np.save(relpath(main + 'data/poletti_N0_wls'), wls)
+
+
+# %%
+
+# Save to .dat file for pgfplots
+
+paper_path = relpath(expanduser('~/papers/arf_embedding/\
+figures'))
+
+mask = ~np.isnan(CL)
+mask[14] = False
+
+# both = np.concatenate((es[mask][np.newaxis], CL[mask][np.newaxis]), axis=1)
+both = np.column_stack((wls[mask], CL[mask]))
+# both = np.column_stack((x,y))
+np.savetxt(paper_path + '/fixed_capillaries.dat', both, fmt='%.8f')
---
+++ arf_fiber/wavelength/glass/pml_study/long.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/bash
+#SBATCH --job-name pmlstdy 
+#SBATCH -N 14
+#SBATCH -n 14
+#SBATCH --tasks-per-node 1
+#SBATCH --cpus-per-task 20
+#SBATCH --partition long
+#SBATCH --mem 0
+#SBATCH --output=logs/log.out
+#SBATCH --error=errors/log.out
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=piet2@pdx.edu
+
+# Load needed modules.
+module load ngsolve/serial
+module load gcc-9.2.0
+module load intel
+
+# Run the code.
+echo "Starting convergence study: "
+date
+
+ref=0
+p=5
+alpha=5
+
+for i in {0..199}
+    do
+        module load ngsolve/serial gcc-9.2.0 intel
+        srun --unbuffered --nodes 1 --ntasks 1 \
+            --output="logs/e_${i}_p${p}_task_%s.out" \
+            --error="errors/e_${i}_p${p}_task_%s.err" \
+            --cpus-per-task 20 \
+            python3 driver.py $ref $p $alpha ${i} &
+done
+wait
