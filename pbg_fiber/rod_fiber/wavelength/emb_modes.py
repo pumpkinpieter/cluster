efdecd09ba1135b5862b3975bb03606b3a722248
Pieter Vandenberge <pumpkinpieter@gmail.com>
2023-05-09 13:55:41 -0700

    Setting up PBG rod fiber studies.

---
+++ pbg_fiber/rod_fiber/wavelength/emb_modes.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import os
+import sys
+
+from fiberamp.fiber.microstruct.pbg import PBG
+from fiberamp.fiber.microstruct.pbg.fiber_dicts.rod import params
+
+if not os.path.isdir('outputs'):
+    print('Making directory: outputs')
+    os.makedirs('outputs')
+
+# Set result arrays
+nspan = 4
+betas = np.zeros(nspan, dtype=complex)
+
+# Center interpolation
+wls = np.linspace(.7, 1.2, 400) * 1e-6
+xs = np.array([wls[65], wls[75], wls[100], wls[125],
+              wls[150], wls[175], wls[199]])
+ys = np.array([4.7, 4.786, 4.9, 4.967, 5.018, 5.071, 5.12])
+a, b, c, d = np.polyfit(xs, ys, 3)
+
+# Embedding parameter array
+wls = np.linspace(.7, 1.2, 2000) * 1e-6
+
+# Search centers
+centers = a * wls**3 + b * wls**2 + c * wls + d
+
+# PML strength
+alpha = 5
+
+if __name__ == '__main__':
+
+    ref, p, i = int(sys.argv[1]), int(sys.argv[2]), int(sys.argv[3])
+
+    if len(sys.argv) > 4:
+        L, R = float(sys.argv[4]), float(sys.argv[5])
+        save_index = np.where((L < wls) * (wls < R))[0]
+
+        if not os.path.isdir('modes'):
+            print('Making directory: modes')
+            os.makedirs('modes')
+
+    params['wavelength'] = wls[i]
+
+    a = PBG(params)
+
+    print('\n' + '#'*8 + ' refinement: ' + str(ref) +
+          ', degree: ' + str(p) + ', wavelength: ' + str(wls[i]) +
+          '#'*8 + '\n', flush=True)
+
+    center = centers[i]
+    radius = .02
+    npts = 4
+
+    beta, _, Es, _, _ = a.leakyvecmodes(ctr=center,
+                                        rad=radius,
+                                        alpha=alpha,
+                                        nspan=nspan,
+                                        npts=npts,
+                                        p=p,
+                                        niterations=9,
+                                        nrestarts=0,
+                                        stop_tol=1e-9,
+                                        inverse='pardiso')
+
+    betas[: len(beta)] = beta[:]
+
+    print('method done, saving.\n', flush=True)
+
+    np.save('outputs/wl' + str(i), betas)
+
+    # if len(sys.argv) > 4 and i in save_index:
+    #     a.save_mesh('modes/mesh_e' + str(i))
+    #     a.save_modes(Es, 'modes/mode_e' + str(i))
