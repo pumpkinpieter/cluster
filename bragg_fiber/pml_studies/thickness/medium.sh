f8b4dc755ef494dd5d1d22d8e7c35a2a1114a3db
Pieter Vandenberge <piet2@login1.cluster>
2024-02-28 18:23:26 -0800

    thickness study

---
+++ bragg_fiber/pml_studies/thickness/medium.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/bash
+#SBATCH --job-name bragconv
+#SBATCH -N 21
+#SBATCH -n 21
+#SBATCH --tasks-per-node 1
+#SBATCH --cpus-per-task 20
+#SBATCH --partition medium
+#SBATCH --mem 0
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user piet2@pdx.edu
+#SBATCH --output=logs/log.out
+#SBATCH --error=errors/error.err
+
+# Load needed modules.
+module load ngsolve/myserial
+module load gcc-9.2.0
+module load intel
+
+if [ ! -d "logs" ]; then
+      echo "making log directory."
+      mkdir logs
+  fi
+
+if [ ! -d "errors" ]; then
+      echo "making error directory."
+      mkdir errors
+  fi
+
+p=8
+
+# Run the code.
+echo "Starting convergence study: "
+for i in {0..20}
+    do
+        module load ngsolve/myserial gcc-9.2.0 intel
+        srun --unbuffered --nodes 1 --ntasks 1 \
+           --output="logs/ref0_p${p}_T${i}_task%s.out" \
+           --error="errors/ref0_p${p}_T${i}_task%s.err" \
+           python3 driver.py 0 $p ${i} &
+done
+wait
